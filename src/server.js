ffdcbe73e062dd0e55fee81451268977fa36de79
Myke Silva <119154150+Spike2247@users.noreply.github.com>
2024-11-25 21:31:24 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,9 @@
+ /*                                   Aula (376) Preparando Base 17/08/23*/ 
+
+//(376) O arquivo server.js vai importar a variável APP
+let app = require("./app");
+
+app.listen(3131, () => {
+    console.log("Servidor Rodando")
+})
+ 
